a1580b479c5809b4cfd2915ad77eb45b13657758
ADRIAN MARCOS BETANZOS <zS19017420@estudiantes.uv.mx>
2022-03-16 18:37:44 -0600

    Hello

---
+++ SistemaPrimaria-main/SistemaPrimaria/Models/Materias.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaPrimaria.Models
+{
+    public class Materias
+    {
+        string NombreMateria {get; set;}
+        int Id_materia { get; set; }
+
+        public Materias()
+        {
+
+        }
+
+
+    }
+}
